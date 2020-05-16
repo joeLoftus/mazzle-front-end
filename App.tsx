038c2c0384bc007405d19fa1d73767a0a38b6c1b
loft475 <joseph.loftus@pnnl.gov>
2020-05-15 20:53:55 -0700

    render mock data

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MockData from "./data/mockData.js";
+
+const renderMovies = () => {
+  return MockData.map((movie: any, index: number) => {
+    return <Text key={index}>{movie.title}</Text>;
+  });
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {renderMovies()}
     </View>
   );
 }
@@ -12,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
